be8068a139c6627381a6cc7fcc303161b3d6ab0f
TobiasDemuth <150724275+TobiasDemuth@users.noreply.github.com>
2023-11-29 22:45:55 +0100

    header-hover-fix

--- global-scripts/nav-script.js
+++ global-scripts/nav-script.js
@@ -9,44 +9,72 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const header = document.querySelector('header');
+    const header = document.querySelector('header');
+    const menu = document.querySelector('#menu');
+    const navbar = document.querySelector('.navbar');
+    let timeout;
 
-  function flyOutHeader() {
-      header.style.transform = 'translateY(-98.5%)';
-      header.style.transition = 'transform 1s ease';
-  }
+    function flyOutHeader() {
+        header.style.transform = 'translateY(-98.5%)';
+        header.style.transition = 'transform 1s ease';
+    }
 
-  function flyInHeader() {
-      header.style.transform = 'translateY(0)';
-      header.style.transition = 'transform 0.5s ease';
-  }
+    function flyInHeader() {
+        header.style.transform = 'translateY(0)';
+        header.style.transition = 'transform 0.5s ease';
+    }
 
-  function handleMouseMove(event) {
-      const isMouseOverHeader = event.clientY <= header.clientHeight +20;
+    function resetTimeout() {
+        clearTimeout(timeout);
+        timeout = setTimeout(flyOutHeader, 3000);
+    }
 
-      if (isMouseOverHeader) {
-          flyInHeader();
-      } else {
-          flyOutHeader();
-      }
-  }
+    function handleMouseMove(event) {
+        const isMouseOverHeader = event.clientY <= header.clientHeight + 20;
 
-  function handleScroll() {
-      if (window.scrollY == 0) {
-          flyInHeader();
-      } else {
-          flyOutHeader();
-      }
-  }
+        if (isMouseOverHeader) {
+            flyInHeader();
+            resetTimeout();
+        } else {
+            flyOutHeader();
+        }
+    }
 
-  document.addEventListener('mousemove', handleMouseMove);
-  document.addEventListener('scroll', handleScroll);
+    function handleScroll() {
+        if (window.scrollY <= 50) {
+            flyInHeader();
+            resetTimeout();
+        } else {
+            flyOutHeader();
+        }
+    }
 
-  header.addEventListener('mouseenter', function () {
-      flyInHeader();
-  });
+    function closeMenu() {
+        navbar.classList.remove('open');
+        menu.classList.remove('hover-effect');
+    }
 
-  header.addEventListener('mouseleave', function () {
-      flyOutHeader();
-  });
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('scroll', handleScroll);
+
+    header.addEventListener('mouseenter', function () {
+        flyInHeader();
+    });
+
+    header.addEventListener('mouseleave', function () {
+        resetTimeout();
+    });
+
+    menu.addEventListener('click', function () {
+        navbar.classList.toggle('open');
+        menu.classList.toggle('hover-effect', navbar.classList.contains('open'));
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!navbar.contains(event.target) && event.target !== menu) {
+            closeMenu();
+        }
+    });
+
+    resetTimeout();
 });
