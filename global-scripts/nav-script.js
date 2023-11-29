cafe39157e4e752465fddd7586318019d6c877f7
TobiasDemuth <150724275+TobiasDemuth@users.noreply.github.com>
2023-11-29 17:19:05 +0100

    header-hover-hinzugefügt

--- global-scripts/nav-script.js
+++ global-scripts/nav-script.js
@@ -7,3 +7,46 @@ document.addEventListener('DOMContentLoaded', function() {
       menu.classList.toggle('hover-effect', navbar.classList.contains('open'));
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector('header');
+
+  function flyOutHeader() {
+      header.style.transform = 'translateY(-98.5%)';
+      header.style.transition = 'transform 1s ease';
+  }
+
+  function flyInHeader() {
+      header.style.transform = 'translateY(0)';
+      header.style.transition = 'transform 0.5s ease';
+  }
+
+  function handleMouseMove(event) {
+      const isMouseOverHeader = event.clientY <= header.clientHeight +20;
+
+      if (isMouseOverHeader) {
+          flyInHeader();
+      } else {
+          flyOutHeader();
+      }
+  }
+
+  function handleScroll() {
+      if (window.scrollY == 0) {
+          flyInHeader();
+      } else {
+          flyOutHeader();
+      }
+  }
+
+  document.addEventListener('mousemove', handleMouseMove);
+  document.addEventListener('scroll', handleScroll);
+
+  header.addEventListener('mouseenter', function () {
+      flyInHeader();
+  });
+
+  header.addEventListener('mouseleave', function () {
+      flyOutHeader();
+  });
+});
